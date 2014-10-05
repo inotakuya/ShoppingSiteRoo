b9d1782682e5ea447f43d15996dec4dfec14bdf7
inotaku <i_no_u_u@yahoo.co.jp>
2014-10-06 01:14:34 +0900

    名前検索機能追加

--- ShoppingSiteRoo/src/main/java/jp/com/inotaku/domain/ItemController.java
+++ ShoppingSiteRoo/src/main/java/jp/com/inotaku/domain/ItemController.java
@@ -2,9 +2,11 @@ package jp.com.inotaku.domain;
 import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.roo.addon.web.mvc.controller.finder.RooWebFinder;
 
 @RequestMapping("/items")
 @Controller
 @RooWebScaffold(path = "items", formBackingObject = Item.class)
+@RooWebFinder
 public class ItemController {
 }
